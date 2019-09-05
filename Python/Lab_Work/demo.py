e35dc69ce446ecea8a9fab390f917796aa8dadad
rohithbodagala <47152581+rohithbodagala@users.noreply.github.com>
2019-09-05 21:20:07 +0530

    Create demo.py

---
+++ Python/Lab_Work/demo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+print("Demo: python does not need data declaration")
+age=20
+height=6.15
+name="Munna Bhai"
+print("Age is",age)
+print("Height is",height)
+print("Name is",name)
+age,height,name=51,5.91,"Mugambo"
+print("Age is",age)
+print("Height is",height)
+print("Name is",name)
+age,height,name="Gabbar",5.54,43
+print("Age is",age)
+print("Height is",height)
+print("Name is",name)
