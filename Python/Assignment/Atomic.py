ed95bfb8f27bf4a4a8ffcebb4ce45dbcb752290d
rohithbodagala <47152581+rohithbodagala@users.noreply.github.com>
2019-09-06 00:55:10 +0530

    Add files via upload

---
+++ Python/Assignment/Atomic.py
@@ -0,0 +1,23 @@
+def AtomicDictionary():
+	atoms={
+		'H':'Hydrogen',
+		'He':'Helium',
+		'Li':'Lithium',
+		'B':'Boron',
+		'C':'Carbon',
+		'N':'Nitrogen'
+	}
+	print(atoms.items())
+	print("Enter unique and duplicate element to the dictionary")
+	print("Atomic Symbol:")
+	x=input()
+	print("Atomic Name:")
+	y=input()
+	atoms[x]=y
+	print(atoms.items())
+	print("Length of the dictionary is",len(atoms))
+	print("Enter a key to search")
+	z=input()
+	print(atoms[z])
+AtomicDictionary()
+
