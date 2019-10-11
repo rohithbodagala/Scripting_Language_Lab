688b554c9e0367515f6ae1301f43ef6ce37fdc84
rohithbodagala <47152581+rohithbodagala@users.noreply.github.com>
2019-10-11 10:06:26 +0530

    Update AgeConvert.py

--- Python/Assignment/AgeConvert.py
+++ Python/Assignment/AgeConvert.py
@@ -1,7 +1,7 @@
 def AgeConvert(a):
     a= a.split("-")
-    age = 2019 - int(num[2])
-    if int(num[1])>10:
+    age = 2019 - int(a[2])
+    if int(a[1])>10:
         age = age - 1
     print(age)
 print("Enter the date of birth")
