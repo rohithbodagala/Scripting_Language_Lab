48e8e988c4d6cf8418bebb922db9a3a63e4e7596
rohithbodagala <47152581+rohithbodagala@users.noreply.github.com>
2019-10-11 10:19:48 +0530

    Add files via upload

---
+++ lab8/Assignment/BetterSurround.py
@@ -0,0 +1,23 @@
+def betterSurround(st):
+    st = list(st)
+    
+    if st[0]!= '+' and st[len(st)-1]!= '=':
+        return False
+    else:
+        for i in range(0,len(st)):
+            if st[i].isalpha():
+                if not (st[i-1]=='+' or st[i-1]=='=') and (st[i+1] == '+' or st[i+1] == '='):
+                    return False
+            else:
+                if i!=0 and i!= len(st)-1:
+                    if st[i-1].isalpha() and st[i+1].isalpha():
+                        return False
+    return True
+st=input()
+new=betterSurround(st)
+if new:
+    print("y")
+else:
+    print("N")
+
+
---
+++ lab8/Assignment/ChangeString.py
@@ -0,0 +1,16 @@
+def ChangeString(st):
+    st = list(st)
+    
+    for i in range(0,len(st)):
+        if st[i].isalpha():
+            if st[i] == 'Z':
+                st[i] = 'a'
+            else:
+                st[i] = (chr(ord(st[i]) + 1))
+#     print(st)
+    for i in range(0,len(st)):
+        if st[i] in ['a','e','i','o','u']:
+            st[i] = st[i].upper()
+    print("".join(st))
+st=input()
+ChangeString(st)
